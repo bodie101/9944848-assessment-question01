a5202f949df8920356ce2af52c82d088fb534b2a
bodie101 <keysbw@g.toiohomai.ac.nz>
2017-10-24 10:46:28 +1300

    inital commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace assessment31
+{
+    class Program
+    {
+        static List<string> falcons = new List<string> { "Hisan", "Ka kite", "Siren", "koa", "Stella", "mojo" };
+        static List<string> falconers = new List<string> { "Debbie", "Heidi", "Noel", "Melissa", "Kirsten", "koshy" };
+
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            Console.WriteLine(DisplayFalconers());
+            
+        }       
+
+        static int GenerateRandomNumber(List<string> myList)
+        {
+            var rand = new Random();
+            return rand.Next(1, myList.Count);
+        }
+
+        static string DisplayFalconers()
+        {
+
+            var selectedFalcons = falcons[GenerateRandomNumber(falcons)];
+            var selectedFalconers = falconers[GenerateRandomNumber(falconers)];
+
+            
+            return $"Selected Falconer = {selectedFalconers} - Selected Falcon = {selectedFalcons}";
+        }
+
+    }
+}
